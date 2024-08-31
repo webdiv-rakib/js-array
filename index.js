e91f950ab9ee379bbfc0e54a0adf3ab235347871
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-31 18:09:24 +0600

    examle of index

---
+++ index.js
@@ -0,0 +1,18 @@
+const numbers = [88, 77, 55, 22, 11];
+
+// to show array elements
+console.log(numbers);
+
+// to show elements value by index number
+console.log(numbers[4]);
+
+// index value to a variable
+const fourth = numbers[3];
+console.log(fourth);
+
+// set or update element value by index
+let abc = 45;
+abc = 50;
+
+numbers[1] = 51;
+console.log(numbers);
