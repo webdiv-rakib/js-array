ebf17ed6abca4dccb0ce6fc3b15ed71efe22996a
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-31 19:01:06 +0600

    example of isArray

---
+++ isArray.js
@@ -0,0 +1,12 @@
+const friends = ['Sabbir', 'Rafi', 'Zihad', 'Kamrul', 'Towhid', 'Rashed'];
+
+const nums = [];
+
+const food = 'ros o gol l a';
+
+const age = 15;
+
+console.log(Array.isArray(friends))
+console.log(Array.isArray(nums))
+console.log(Array.isArray(food))
+console.log(Array.isArray(age))
