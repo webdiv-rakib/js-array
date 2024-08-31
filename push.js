b18eb5a7fd33da850d53ec8c96758e76f2607c75
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-31 18:21:11 +0600

    example of push

---
+++ push.js
@@ -0,0 +1,32 @@
+const ages = [];
+const numbers = [2, 5, 7, 9, 12, 15];
+console.log(numbers);
+
+// push elements to the array
+numbers.push(20);
+numbers.push(45);
+// push multiple elements to the array
+numbers.push(50, 61, 66);
+console.log(numbers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[3]);
+
+// const fifth = numbers[5];
+// console.log(fifth);
+
+// numbers[1] = 3;
+// console.log(numbers);
