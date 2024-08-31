579baa8a88e30cbabe2ece1dda8505b37ee1b352
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-31 18:42:11 +0600

    example of shift - unshift

---
+++ shift-unshift.js
@@ -0,0 +1,7 @@
+const friends = ['Sabbir', 'Rafi', 'Zihad', 'Kamrul', 'Towhid', 'Rashed'];
+console.log(friends);
+friends.shift()
+console.log(friends);
+
+friends.unshift('Ghumailam')
+console.log(friends);
