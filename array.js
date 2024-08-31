7c0c052ec7b4f9087d498a3860a1b31a8693714d
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-31 17:42:06 +0600

    examples of array

--- array.js
+++ array.js
@@ -1,1 +1,8 @@
-// 
+// const num1 = 12;
+// const num2 = 22;
+// const num3 = 33;
+
+const numbers = [12, 22, 33, 44];
+const friends = ['abu', 'nayim', 'mohammad'];
+const passed = [true, false, true, false];
+const mixedArray = [12, 'Tangail', true, 45.56];
