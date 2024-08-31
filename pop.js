8d8b27d6cee5a7f8304e327cbc84c67741f50902
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-31 18:32:45 +0600

    example of using pop

---
+++ pop.js
@@ -0,0 +1,14 @@
+const friends = ['Sabbir', 'Rafi', 'Zihad', 'Kamrul', 'Towhid', 'Rashed'];
+console.log(friends);
+
+// to remove last elements from the array
+const out1 = friends.pop();
+console.log(friends);
+
+const out2 = friends.pop();
+console.log(friends);
+
+const out3 = friends.pop();
+console.log(friends);
+
+console.log(out2,out3);
