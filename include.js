f598c6e40bcb59c1b86dd6183dbc47b70d283665
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-31 18:50:11 +0600

    example of include

---
+++ include.js
@@ -0,0 +1,11 @@
+const friends = ['Sabbir', 'Rafi', 'Zihad', 'Kamrul', 'Towhid', 'Rashed'];
+
+console.log(friends.includes('gelam'))
+console.log(friends.includes('Sabbir'))
+
+if (friends.includes('ghumailam')){
+    console.log('Shuilam');
+}
+else{
+    console.log('Kichui kori nai');
+}
