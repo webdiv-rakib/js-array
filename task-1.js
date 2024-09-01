c462c5ea8c1adceb398ebfb8e761f87b7a1a1829
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-01 17:52:55 +0600

    task-1 question added

--- task-1.js
+++ task-1.js
@@ -1,3 +1,10 @@
+/**1. Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+*/
+
 const fruites = ['amm', 'jam', 'lichu', 'kola', 'kathal']
 console.log(fruites);
 console.log(fruites[3]);
---
+++ task-2.js
@@ -0,0 +1,9 @@
+const area = ['Dhaka', 'Sylhet', 'Cox-bazar']
+console.log(area);
+area.push('Tangail')
+console.log(area);
+area.push('Chittagong')
+area.push('Rajshahi')
+console.log(area);
+area.pop()
+console.log(area);
