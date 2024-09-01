d2cde3bd4a90edb0901347b7054680dd97c441aa
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-01 17:55:22 +0600

    task-2

--- task-1.js
+++ task-1.js
@@ -1,8 +1,8 @@
 /**1. Declare an array
-Declare an array with 5 elements containing fruits
-console log the 3rd index element
-change the value of the 2nd index element to jambura
-console log the final array
+    1. Declare an array with 5 elements containing fruits
+    2. console log the 3rd index element
+    3. change the value of the 2nd index element to jambura
+    4. console log the final array
 */
 
 const fruites = ['amm', 'jam', 'lichu', 'kola', 'kathal']
--- task-2.js
+++ task-2.js
@@ -1,3 +1,12 @@
+/**2. Add or remove elements
+ * 
+    1. Declare an array of 3 tourist destinations
+    2. Add a new tourist destination to your tourist array
+    3. Add two more to your array
+    4. Remove the last tourist destination you have added
+    5. display the final array as output
+*/
+
 const area = ['Dhaka', 'Sylhet', 'Cox-bazar']
 console.log(area);
 area.push('Tangail')
