aa893856b6984660660a87f8549085aa746b8905
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-01 18:18:46 +0600

    task-5

---
+++ task-5.js
@@ -0,0 +1,14 @@
+/**5. Combining Arrays
+    Instructions:
+
+    1. Create two arrays of your choice.
+    2. Use the concat method to combine the two arrays into a new array.
+    3. Print both the original arrays and the combined array using console.log().
+*/
+
+const names = ['rakib', 'sabbir', 'rafi']
+const numbers = ['tangail', 'bogura', 'nowga']
+const join = names.concat(numbers);
+
+console.log(names,numbers);
+console.log(join);
